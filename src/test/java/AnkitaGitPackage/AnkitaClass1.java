72481e3a443649a09efc923dc0711b8d5571dbb7
Ankita <ankitarai537@gmail.com>
2021-06-13 13:29:25 +0530

    Adding new java class

---
+++ src/test/java/AnkitaGitPackage/AnkitaClass1.java
@@ -0,0 +1,14 @@
+package AnkitaGitPackage;
+
+public class AnkitaClass1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Anshu");
+		System.out.println("Princi");
+		System.out.println("Roshan");
+		System.out.println("Ankita");
+
+	}
+
+}
