f17c08268a41752bc94022e0dc0bc9b18e38a214
Ankita <ankitarai537@gmail.com>
2021-06-13 20:08:25 +0530

    adding wild animal

--- src/test/java/AnkitaGitPackage/AnkitaClass.java
+++ src/test/java/AnkitaGitPackage/AnkitaClass.java
@@ -4,7 +4,8 @@ public class AnkitaClass {
 	public static void main(String args[]) {
 		System.out.println("Fruits");
 		System.out.println("Vegetable");
-		System.out.println("Birds");
+		//System.out.println("Birds");
+		System.out.println("Wild Animal");
 		
 	}
 
