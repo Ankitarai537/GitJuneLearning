3888ac491275b9801e2aa1a19eaa66102bec73b9
Ankita <ankitarai537@gmail.com>
2021-06-14 13:36:59 +0530

    Adding PrinciUI created and now commiting in PrinciUI in princi UI

--- src/test/java/AnkitaGitPackage/AnkitaClass.java
+++ src/test/java/AnkitaGitPackage/AnkitaClass.java
@@ -5,6 +5,7 @@ public class AnkitaClass {
 		System.out.println("Fruits");
 		System.out.println("Vegetable");
 		System.out.println("AnkitaUI add");
+		System.out.println("PrinciUI created and now commiting in PrinciUI");
 
 	}
 
